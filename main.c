55450f6555fd01c5768fdec8b2e53277378ae7d6
RorySuth <rorysuther@gmail.com>
2017-05-17 21:32:28 +1000

    1.2
    Sleep mode now works
    Wakes up on Button 1 press and displays "Calc" then "read" and goes back to sleep.

--- main.c
+++ main.c
@@ -18,11 +18,19 @@ int displayNumber;
 int displayPosition = 0;    //The digit that the display starts from
 int displayDigits = 4;      //The number of digits on the 7-segment display
 
+/*----------Flags--------------------*/
+int sleep_flag = 0;         // Used to tell functions whether the device is asleep
+int sleep_flash = 0;        // Used to tell the decimal point to flash in sleep mode
+int measure_flag = 0;       // Used to tell the device when to measure heart rate
+
+
 /*----------Function Prototypes------*/
 void Start();
 
 /*----------Interrupt Prototypes-----*/
 CY_ISR_PROTO(sleep_isrHandler);
+CY_ISR_PROTO(button_isrHandler);
+CY_ISR_PROTO(display_isrHandler);
 
 
 
@@ -36,11 +44,16 @@ int main(void)
     Start();
     for(;;)
     {
-        
-        LED_Driver_Write7SegNumberDec(displayNumber, displayPosition, displayDigits, LED_Driver_RIGHT_ALIGN);
-        /*Test*/
-        CyDelay(100);
-        displayNumber = displayNumber + 1;
+        if (measure_flag) {
+            LED_Driver_WriteString7Seg("calc",0);
+            // TODO: Write function to calibrate LED brightness 
+            CyDelay(2000);
+            LED_Driver_WriteString7Seg("read",0);
+            CyDelay(2000);
+            // TODO: Write function to take heart rate measurement
+            measure_flag = 0;
+            sleep_isr_SetPending();                         //Puts the device to sleep
+        }
         
     }
     
@@ -52,6 +65,8 @@ void Start() {//start up flashing sequence
         EEPROM_Enable();
         LED_Driver_Start();
         sleep_isr_StartEx(sleep_isrHandler);
+        button_isr_StartEx(button_isrHandler);
+        display_isr_StartEx(display_isrHandler);
         
         
         LED_Driver_ClearDisplayAll();
@@ -70,6 +85,9 @@ void Start() {//start up flashing sequence
         LED_Driver_SetDisplayRAM((uint8) 0xFF, 3);   
         CyDelay(1000);
         
+        sleep_timer_Start();                            //Allows the startup sequence to run without being interrupted by the sleep isr
+        Display_timer_Start();
+        sleep_isr_SetPending();                         //Puts the device to sleep
 }
 
 /*-----------------Interrupts--------------*/
@@ -79,23 +97,47 @@ CY_ISR(sleep_isrHandler)
         /* Clear pending Interrupt */
         sleep_isr_ClearPending();
         sleep_timer_ReadStatusRegister();
+        display_isr_ClearPending();     //Stops the display updating after this interrupt has run
+        Display_timer_ReadStatusRegister();
         
         /* Code */
-//        Timer_1_Sleep();
-//        Timer_2_Sleep();
-//        sleep_flash_timer_Wakeup(); //Wakes up the flashing function for sleep mode
-//        segA_Write(1);
-//        segB_Write(1);
-//        segC_Write(1);
-//        segD_Write(1);
-//        segE_Write(1);
-//        segF_Write(1);
-//        segG_Write(1);
-//        segDec_Write(0);
-//        digit1_Write(1);
-//        digit2_Write(0);
-//        digit3_Write(1);
-//        digit4_Write(1);
+        LED_Driver_ClearDisplayAll(); //Sets all display segments blank
+        LED_Driver_SetDisplayRAM((uint8) 0x80, 1);
+        sleep_flag = 1;               //Tells the device it is now asleep
+        sleep_timer_Stop();           //Puts the timer calling the sleep isr to sleep so sleep isr isn't triggering every 2 seconds while device is sleeping
+        
+    }
+    
+CY_ISR(button_isrHandler)
+    {
+        /* Clear pending Interrupt */
+        button_isr_ClearPending();
+        Button_1_ClearInterrupt();
+        
+        /* Code */
+        sleep_flag = 0;                 //Tells the device it is now awake
+        measure_flag = 1;               //Tells the device to start measuring the heart rate
+
+    }
+    
+CY_ISR(display_isrHandler)
+    {
+        /* Clear pending Interrupt */
+        display_isr_ClearPending();
+        Display_timer_ReadStatusRegister();
+        
+        /* Code */
+        if (sleep_flag) {
+            if (sleep_flash){
+                LED_Driver_ClearDisplayAll();
+                sleep_flash = 0;
+            }
+            else {
+                LED_Driver_SetDisplayRAM((uint8) 0x80, 1);
+                sleep_flash = 1;
+            }
+        }
+        
         
     }
 
